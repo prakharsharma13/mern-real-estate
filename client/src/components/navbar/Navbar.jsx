645e132d57852d7f34b02ca55b66c5e359b8a565
prakharsharma13 <115884239+prakharsharma13@users.noreply.github.com>
2024-06-20 17:32:43 +0530

    Navbar Design

---
+++ client/src/components/navbar/Navbar.jsx
@@ -0,0 +1,24 @@
+import "./navbar.scss";
+
+function Navbar(){
+    return(
+        <nav>
+            <div className="left">
+                <a href="/" className="logo">
+                    <img src="/logo.png" alt="" />
+                    <span>RealEstate</span>
+                </a>
+                <a href="/">Home</a>
+                <a href="/">About</a>
+                <a href="/">Contact</a>
+                <a href="/">Agents</a>
+            </div>
+            <div className="right">
+                <a href="/">SignIn</a>
+                <a href="/" className="register">SignUp</a>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar;
